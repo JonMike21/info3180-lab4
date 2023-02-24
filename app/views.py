291ae17fc0431daa7d465bd9207a8d4a86ab6048
Jon-Michael <jof8888@gmail.com>
2023-02-23 22:43:28 -0500

    Completed Exercise 6

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort,send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
@@ -90,6 +90,32 @@ def login():
 @login_manager.user_loader
 def load_user(id):
     return db.session.execute(db.select(UserProfile).filter_by(id=id)).scalar()
+
+
+def get_upload_images():
+    dir = os.getcwd()
+    #print(dir)
+    fname=[]
+    for subdir, dirs, files in os.walk(dir + '/uploads'):
+        for file in files:
+            if file.endswith('.jpg') or file.endswith('.png') or file.endswith('.jpeg'):
+                fname.append(file)
+                #print (os.path.join(subdir, file))
+    #print (get_upload_images())
+    return fname
+
+
+
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
+    
+
+
+@app.route('/files')
+@login_required
+def files():
+    return render_template('files.html',images=get_upload_images())
 
 ###
 # The functions below should be applicable to all Flask apps.
